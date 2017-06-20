e6114080310ad635775e6a1a0e9df085e28be043
Venkat <itsmygalaxy@users.noreply.github.com>
2017-06-20 16:12:58 -0500

    Create len_count.py

---
+++ General/len_count.py
@@ -0,0 +1,13 @@
+#Program to find the length and count the occurences of a particular letter.
+__author__ = "itsmygalaxy"
+s = "itsmygalaxy"
+
+# This will print the lengh of the string.
+print(len(s)) 
+
+# This will give you how many times the "my" occurred from the begining "0" to the end which is the length of the string.
+print s.count ("my",0,len(s))
+
+
+
+
