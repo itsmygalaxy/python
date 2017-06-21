7eed7fc9c8866462c53c6ade074bc877d93c24d6
Venkat <itsmygalaxy@users.noreply.github.com>
2017-06-21 16:02:41 -0500

    Create encode_decode.py

---
+++ General/encode_decode.py
@@ -0,0 +1,15 @@
+# Encoding and Decoding values
+__author__ = "Venkat"
+s="itsmygalaxy"
+s = s.encode('base64','strict')
+print(s)
+
+s = s.decode('base64','strict')
+print(s)
+
+======================
+#python encode_decode.py
+aXRzbXlnYWxheHk=
+
+itsmygalaxy
+======================
