b6bded247807acf617b41823b2f8c6036ad28d0a
Venkat <itsmygalaxy@users.noreply.github.com>
2017-06-25 10:55:10 -0500

    Create if_elif_else.py

---
+++ General/if_elif_else.py
@@ -0,0 +1,11 @@
+__author__ = "Venkat"
+num = input('Enter a number between 1-10:\n')
+print type(num)
+if num < 10:
+    print"num value is less than 10"
+elif num == 10:
+    print"num value is neither less nor greater than 10(so its 10)"
+else:
+    print"num value is greater than 10"
+    
+    
