fc1e0114d4e096ad4a2ad6516665307a3faa6394
Venkat <itsmygalaxy@users.noreply.github.com>
2017-06-30 13:30:07 -0500

    Comments
    The for..in statement is another looping statement which iterates over a sequence of objects. i.e go thr' each item in sequence.

---
+++ General/for_loop.py
@@ -0,0 +1,21 @@
+__Author__ = "Venkat"
+# for loop
+for i in 'Itsmygalaxy':
+        print 'The current letter:', i
+
+
+'''
+############Output###############
+The current letter: I
+The current letter: t
+The current letter: s
+The current letter: m
+The current letter: y
+The current letter: g
+The current letter: a
+The current letter: l
+The current letter: a
+The current letter: x
+The current letter: y
+##########End of Output############
+'''
