16e36442fb6a4e6a9d767296911d89e638aba872
Venkat <itsmygalaxy@users.noreply.github.com>
2017-06-30 16:39:55 -0500

    Create for_else_loop.py

---
+++ General/for_else_loop.py
@@ -0,0 +1,11 @@
+__Author__ = "Venkat"
+#for 
+list = [1,2,3,4,5]
+for i in list:
+        if i == 6:
+                print'object found',i
+else:
+        print'No object found'
+###########Output###########
+No object found
+########End of Output#######
