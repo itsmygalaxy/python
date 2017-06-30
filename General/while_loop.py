ac07717626c11dee57c8655e68ca10bf214c218b
Venkat <itsmygalaxy@users.noreply.github.com>
2017-06-30 11:08:16 -0500

    Create while_loop.py

---
+++ General/while_loop.py
@@ -0,0 +1,13 @@
+__Author__ = "Venkat"
+i = 0
+while i < 3:
+        print 'the value of i is', i
+        i += 1
+
+'''
+###########Output###########
+the value of i is 0
+the value of i is 1
+the value of i is 2
+########End of Output#######
+'''
