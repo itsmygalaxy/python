ee5575a84721535dc90c95a26253165c150960da
Venkat <itsmygalaxy@users.noreply.github.com>
2017-07-03 01:33:48 -0500

    Create range.py

---
+++ General/range.py
@@ -0,0 +1,21 @@
+__Author__ = "Venkat"
+for i in range(5):
+  print i
+  
+  '''
+  ######Output#####
+0
+1
+2
+3
+4
+#####End of Output#####
+'''
+
+#The other ways of range can be used as:
+for i in range(15):
+    print '10.102.1.'+str(i)
+
+
+for i in range (128,129,1):
+    print 'ping -c 2 192.168.1.' +str(i)
